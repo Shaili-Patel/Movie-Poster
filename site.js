5e288323547a17da3e1ee0664f6cec0ef90d20ae
Shaili Patel <526shailipatel@frhsd.com>
2025-12-08 13:26:38 -0500

    Create a variable for a title, owner, and a GitHub repository link.

--- site.js
+++ site.js
@@ -20,28 +20,26 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
+    created () {
+        fetch('movies.json')
+            .then(response => response.json())
+            .then(json => {
+                this.movies = json;
+            });
     },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+    data() {
+        return {
+            movies: [], // this will hold your movies.json data
+            title: "2000's Chick Flick Diaries",  
+            owner: "Shaili Patel",                
+            github: "https://github.com/Shaili-Patel/Movie-Poster"  
+        };
+    },
+    methods: {
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+    }
+});
 
-vue_app.mount("#vue_app")
-
+vue_app.mount("#vue_app");
 
